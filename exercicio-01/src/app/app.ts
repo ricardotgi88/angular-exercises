977c421ac4657365eb69c845d98db1f8da89303f
Ricardo Henrique dos Santos <ricardotgi88@hotmail.com>
2025-12-01 18:48:58 +0000

    add pokenode-ts package

--- exercicio-01/src/app/app.ts
+++ exercicio-01/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PokemonClient } from 'pokenode-ts';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('exercicio-01');
+
+  constructor(){
+    const api = new PokemonClient();
+    api
+    .getPokemonByName('luxray')
+    .then((data) => console.log(data)) // will output "Luxray"
+    .catch((error) => console.error(error));
+  }
 }
