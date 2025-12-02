4521d208a1e6d85e3f25a008955a2659862f127b
Ricardo Henrique dos Santos <ricardotgi88@hotmail.com>
2025-12-02 18:26:33 +0000

    add async properties

--- exercicio-01/src/app/app.ts
+++ exercicio-01/src/app/app.ts
@@ -1,15 +1,31 @@
+import { AsyncPipe, CurrencyPipe, JsonPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PokemonClient } from 'pokenode-ts';
+import { BehaviorSubject, combineLatest, concatMap, delay, from, map, of, startWith, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, JsonPipe, CurrencyPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('exercicio-01');
+  public fromArray$ = from([1, 2, 3, 4, 5]).pipe(
+    map((v) => v * 10),
+    delay(5000)
+  );
+
+  public fromArrayRealDelay$ = from([1,2,3,4,5]).pipe(
+  concatMap((v, i) => of(v).pipe(delay(i * 1000))), // 0s, 5s, 10s, ...
+  map(v => v * 10)
+);
+
+  public subje$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  public object = { a: 1, b: 2, c: 3 };
+
+  public valor: number = 0;
 
   constructor(){
     const api = new PokemonClient();
@@ -17,5 +33,43 @@ export class App {
     .getPokemonByName('luxray')
     .then((data) => console.log(data)) // will output "Luxray"
     .catch((error) => console.error(error));
+
+    const array = [1, 2, 3, 4, 5];
+
+    const obs = array.map((num) => of(num).pipe(delay(1000 * num), startWith(0)));
+
+    const comb = combineLatest(obs);
+
+    setTimeout(() => {
+      console.log('--- Subscribing now');
+      comb.subscribe((values) => {
+        console.log(values, '--- second Values');
+      });
+    }, 2000);
+
+    comb.subscribe((values) => {
+        console.log(values, '--- first Values');
+    });
+
+
+    of(array).pipe(
+      delay(1000),
+    )
+    .subscribe((value) => {
+      console.log(value, '--- of Values');
+    });
+    
+    from(array).pipe(
+      delay(1000),
+    )
+    .subscribe((value) => {
+      console.log(value, '--- from Values');
+    });
+
+    this.fromArray$.subscribe((value) => {
+      this.valor = value;
+    });
+
+    this.subje$.next(10);
   }
 }
